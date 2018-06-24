9bec69713b109e2266c11599ae57e3515db21e5b
robled <robled@electronsweatshop.com>
2018-06-24 16:00:57 -0400

    add dlpatch.bat scripts for bl2 and tps

---
+++ bl2/dlpatch.bat
@@ -0,0 +1,2 @@
+powershell -command ^
+"wget https://raw.githubusercontent.com/DontCamp/borderlands/master/bl2/Patch.txt -OutFile \"$HOME\Documents\My Games\Borderlands 2\Binaries\Patch.txt\""
---
+++ tps/dlpatch.bat
@@ -0,0 +1,2 @@
+powershell -command ^
+"wget https://raw.githubusercontent.com/DontCamp/borderlands/master/tps/Patch.txt -OutFile \"$HOME\Documents\My Games\Borderlands The Pre-Sequel\Binaries\Patch.txt\""
